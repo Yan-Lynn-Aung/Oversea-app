6ac2d6be362fcff24e41626590773b8ba96cc093
YanLynn <yan.lynn1991@gmail.com>
2020-06-12 10:19:51 +0630

    master

--- routes/recruiterApi.php
+++ routes/recruiterApi.php
@@ -5,7 +5,8 @@ use Illuminate\Support\Facades\Route;
 
 
 Route::group(['middleware' => ['auth:api']], function() {
-    Route::get('post','PostController@index');
+    
    
 });
 
+Route::get('post','PostController@index');
